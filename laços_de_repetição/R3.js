3c1bf73def3377acc5cd4561b5f8d05b56c02f6f
Guajajara <chrystyanjose16@gmail.com>
2025-06-26 16:24:07 -0300

    R1

---
+++ laços_de_repetição/R3.js
@@ -0,0 +1,5 @@
+//Use for...of para percorrer e exibir cada fruta do array do exercício 15.
+let frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+for (let fruta of frutas) {
+    console.log(fruta);
+}
