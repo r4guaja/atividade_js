9ec445253cfaa4dd27612440264aed8fc4b0e806
Guajajara <chrystyanjose16@gmail.com>
2025-06-26 16:12:30 -0300

    con1

---
+++ condicionais/con1.js
@@ -0,0 +1,5 @@
+//Crie uma variável nota = 7. Escreva uma condicional para exibir “Aprovado” se a nota for maior ou igual a 6.
+let nota = 7;
+if (nota >= 6) {
+  console.log("Aprovado");
+}
---
+++ condicionais/con2.js
@@ -0,0 +1,7 @@
+//Crie uma variável hora = 14. Exiba “Boa tarde” se for entre 12 e 18, senão exiba “Fora do horário da tarde”.
+let hora = 14;
+if (hora >= 12 && hora < 18) {
+  console.log("Boa tarde");
+} else {
+  console.log("Fora do horário da tarde");
+}
