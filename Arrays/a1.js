3f78c46c856d6adcb7a1a9089ebcc6e0af3132bf
Guajajara <chrystyanjose16@gmail.com>
2025-06-26 16:19:43 -0300

    a1

---
+++ Arrays/a1.js
@@ -0,0 +1,12 @@
+let frutas = ["maçã", "banana", "laranja", "uva", "manga"];
+console.log(frutas[0]);
+console.log(frutas[frutas.length - 1]);
+
+frutas.push("kiwi");
+console.log(frutas);
+
+frutas.shift();
+console.log(frutas);
+
+frutas.sort();
+console.log(frutas);
