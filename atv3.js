1730f55593bb116cc42ac66b4ec508ebc45db6bb
Guajajara <chrystyanjose16@gmail.com>
2025-06-26 15:49:27 -0300

    atv3

---
+++ atv3.js
@@ -0,0 +1,3 @@
+//crie uma variavel estaestudando e atribua true ou false
+let estaEstudando = true;
+console.log(estaEstudando);
---
+++ atv4.js
@@ -0,0 +1,4 @@
+//use typeof para verificar o tipo de cada variavel
+
+let boa_noite = true;
+console.log(typeof boa_noite);
